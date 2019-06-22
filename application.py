ed5eb5e8dd3d08932aae46f13da182e51f410572
Nisarg2104 <nisargvora2104@gmail.com>
2019-06-22 18:45:19 +0530

    added feedback page and function in flask

--- application.py
+++ application.py
@@ -1,8 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+from flask_session import Session
+
 app=Flask(__name__)
+
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+feedbacks=[]
+
+@app.route("/feedback",methods=["GET","POST"])
+def feedback_function():
+    if request.method == "POST":
+        my_feedback=request.form.get("feedback")
+        feedbacks.append(my_feedback)
+    
+    return render_template("feedback.html",feedbacks=feedbacks)
+ 
 
